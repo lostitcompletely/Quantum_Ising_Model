d34e514c5e2fcb27ba4f2229d25278af425a78f6
lostitcompletely <yusufuddin2005@yahoo.co.uk>
2026-08-31 00:56:27 +0100

    adding source code for lattice header file, adding first part of code to compute hamiltonian, code to select microstates at random for the MCS and bulding CNN to find the wavefunction still required

---
+++ lattice.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include "qubit.h"
+
+class Lattice {
+    public:
+        double J;
+        double T;
+        std::vector<std::vector<Qubit>> lattice;
+        Lattice(int n_columns, int n_rows, double J, double T){
+            this -> J = J;
+            this -> T = T;
+            double beta = 1.0 / T;
+            //std::vector<std::vector<Qubit>> lattice;
+            for (int i = 0; i < n_columns; i++){
+                std::vector<Qubit> row;
+                for (int j = 0; j < n_rows; j++){
+                    row.push_back(Qubit(1.0,1.0));
+                };
+                lattice.push_back(row);
+            };
+        };
+
+        std::vector<std::vector<int>> get_neighbours(int i, int j){
+            std::vector<std::vector<int>> neighbours;
+            int n_columns = lattice.size();
+            int n_rows = lattice[0].size();
+            neighbours.push_back({(i-1+n_columns)%n_columns, j});
+            neighbours.push_back({(i+1)%n_columns, j});
+            neighbours.push_back({i, (j-1+n_rows)%n_rows});
+            neighbours.push_back({i, (j+1)%n_rows});
+            return neighbours;
+        };
+
+        void switch_neighbours(int i1, int j1, int i2, int j2){
+            Qubit temp = lattice[i1][j1];
+            lattice[i1][j1] = lattice[i2][j2];
+            lattice[i2][j2] = temp;
+        };
+
+        Qubit flip_spin(Qubit& qubit){
+            std::vector<std::vector<int>> M = {{0,1}, {1,0}};
+            for (int i=0; i<2; i++){
+                double new_alpha = M[i][0]*qubit.get_state()[0] + M[i][1]*qubit.get_state()[1];
+                double new_beta = M[i][0]*qubit.get_state()[1] + M[i][1]*qubit.get_state()[0];
+                qubit.set_state(new_alpha, new_beta);
+            };
+            return qubit;
+        }
+
+        int CNOT(Qubit &control,int i, int j){
+            if (control.measure_spin() == 1){
+                flip_spin(control);
+                return 1;
+            } else {
+                return -1;
+            }
+        };
+
+        double hamiltonian_z(std::vector<std::vector<int>> parities){
+            std::vector<double> hamiltonian;
+            double e_z = 0.0;
+            for (int i=0; i<parities.size(); i++){
+                for (int j=0; j<parities.size(); j++){
+                    std::vector<std::vector<int>> neighbours = get_neighbours(i, j);
+                    for (int k=0; k<neighbours.size(); k++){
+                        int ni = neighbours[k][0];
+                        int nj = neighbours[k][1];
+
+                        e_z = e_z + (-J*parities[i][j]*parities[ni][nj]);
+                    };
+                };
+            };
+            return e_z;
+        };
+
+        double hamiltonian_x(std::vector<std::vector<Qubit>> qubits){
+            double e_x = 0.0;
+            std::vector<std::vector<double>> M = {{0,1}, {1,0}};
+            for (int i=0; i<qubits.size(); i++){
+                for (int j=0; j<qubits.size(); j++){
+                    std::vector<std::vector<Qubit>> duplicate = qubits;
+                    duplicate[i][j] = flip_spin(duplicate[i][j]);
+                };
+            };
+            return e_x;
+        };
+};
--- lattice.h
+++ lattice.h
@@ -10,12 +10,13 @@ class Lattice {
         double beta;
         double h;
         std::vector<std::vector<int>> get_neighbours(int i, int j);
+        std::vector<std::vector<Qubit>> lattice;
         void switch_neighbours(int i1, int j1, int i2, int j2);
-        std::vector<Qubit> lattice;
-        void flip_spin(Qubit& qubit);
+        Qubit flip_spin(Qubit& contol, int i, int j);
+        int CNOT(Qubit& control, Qubit& qubit);
         std::vector<int> parity_check();
-        std::vector<double> hamiltonian_z(std::vector<int> parities);
-        std::vector<double> hamiltonian_x(std::vector<int> parities);
+        double hamiltonian_z(std::vector<std::vector<int>> parities);
+        double hamiltonian_x(std::vector<std::vector<Qubit>> qubits);
         double calculate_energy();
         double calculate_weight();
 };
